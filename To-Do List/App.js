ea5494092fec9d935b3d12d8079b458097701936
Sagar Bokade <sagarnbokade@gmail.com>
2024-06-16 19:22:39 +0530

    Added Functionality

--- To-Do List/App.js
+++ To-Do List/App.js
@@ -4,10 +4,12 @@ import './App.css'
 function App() {
   const [todos,setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [isEditing, setIsEditing] =useState(false);
+  const [currentTodo, setCurrentTodo] = useState({});
 
   const addTodo = () =>{
     if(inputValue.trim()){
-      setTodos([...todos,inputValue.trim()]);
+      setTodos([...todos,{ text : inputValue.trim(),isCompleted: false}]);
       setInputValue("");
     }
   };
@@ -15,6 +17,34 @@ function App() {
 
   const deleteTodo = (index) => {
     setTodos(todos.filter((_,i) => i !== index));
+  };
+
+  const editTodo = (index) => {
+    setCurrentTodo({index,text : todos[index].text});
+    setIsEditing(true);
+    setInputValue(todos[index].text);
+  };
+
+  const updateTodo = () => {
+    let updateTodos = todos.map((todo,index) =>{
+      if(index == currentTodo.index){
+        return {...todo,text: inputValue };
+      }
+      return todo;
+    });
+    setTodos(updateTodos);
+    setIsEditing(false);
+    setInputValue("");
+  }
+
+  const toggleComplete = (index) => {
+    let updateTodos = todos.map((todo,i) =>{
+      if(i === index){
+        return {...todo,isCompleted: !todo.isCompleted };
+      }
+      return todos;
+    });
+    setTodos(updateTodos)
   };
 
   return (
@@ -26,11 +56,19 @@ function App() {
       onChange={(e) => setInputValue(e.target.value)}
       placeholder="Add a new Task"
       />
-      <button onClick={addTodo}>Add</button>
+      <button onClick={isEditing ? updateTodo : addTodo}>
+        {isEditing ? "Update" : "Add"}
+      </button>
       <ul>
         {todos.map((todo,index) =>(
           <li key={index}>
-            {todo} <button onClick={(deleteTodo(index))}>Delete</button>
+            <span style={{textDecoration: todo.isCompleted ? "line-through" : "none"}}
+            onClick={() => toggleComplete(index)}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => editTodo(index)}>Edit</button>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
           </li>
         ))
         }
